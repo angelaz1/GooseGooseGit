fb78f52820d6b67e27eaa5c4c0d790b375c1a39d
JDParker714 <jdparker@andrew.cmu.edu>
2020-01-31 22:06:33 -0500

    sdgasdg
    ljshdagfbja

---
+++ GameJam 2020/Assets/Scripts/DialogueNode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace DialogueTree
+{
+    public class DialogueNode
+    {
+        public int NodeID = -1;
+        public string Text;
+        public List<DialogueOption> Options;
+
+        public DialogueNode()
+        {
+            Options = new List<DialogueOption>();
+        }
+
+        public DialogueNode(string text)
+        {
+            Text = text;
+            Options = new List<DialogueOption>();
+        }
+    }
+}
