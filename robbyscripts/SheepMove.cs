e6e6575d0f9ca110865d1da4199bd6579b09445d
Bertoh14 <38470603+Bertoh14@users.noreply.github.com>
2020-01-31 23:53:34 -0500

    Add files via upload

---
+++ robbyscripts/SheepMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepMove : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float offset = 20.0f;
+
+    public GameObject player;
+    public float maxZ;
+    public float gateLeft;
+    public float gateRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EvaluateSheep();
+    }
+
+    void EvaluateSheep()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < 5)
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.x += (currentPosition.x - player.transform.position.x)/10;
+            currentPosition.z += (currentPosition.z - player.transform.position.z) / 10;
+            transform.position = currentPosition;
+        }
+
+        if (transform.position.z > maxZ && transform.position.x > gateLeft && transform.position.x < gateRight)
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.z += .2f;
+            transform.position = currentPosition;
+        }
+    }
+
+}
