7f029c01583a2d14323f2268e56bdb455e43b661
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-02-01 12:00:27 -0500

    Basic camera following, adding bg for office

---
+++ GameJam 2020/Assets/Scripts/Mindscape/Camera_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+	public Transform PlayerTransform;
+
+	private Vector3 _cameraOffset;
+
+	[Range(0.1f, 1.0f)]
+	public float SmoothFactor = 1.0f;
+
+  private float maxDistAway = 5f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+  	_cameraOffset = transform.position - PlayerTransform.position;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {	
+    // Vector3 camPos = new Vector3(transform.position.x, transform.position.y, 0);
+  	// float dist = Vector3.Distance(camPos, PlayerTransform.position);
+  	// if(dist > maxDistAway) {
+    //   Vector3 newPos = transform.position + (dist-maxDistAway) * Vector3.Normalize(PlayerTransform.position - camPos);
+  	// 	// Vector3 newPos = Vector3.MoveTowards(transform.position, PlayerTransform.position, dist-5f);
+  	// 	// Vector3 newPos = PlayerTransform.position + _cameraOffset;
+  	// 	transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor); //newPos;
+  	// }
+
+    Vector3 newPos = PlayerTransform.position + _cameraOffset;
+  	transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+  }
+}
